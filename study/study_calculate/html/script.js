73000453b33535546cc4864c47b84f3ee3d298bb
BangJaehyeok <BangJaehyeok@test.com>
2021-06-02 21:48:03 +0900

    계산기3일차

---
+++ study/study_calculate/html/script.js
@@ -0,0 +1,24 @@
+// 계산기 자바스크립트
+//C버튼 reset기능 추가
+function reset() {
+    document.getElementById("output").innerHTML = "0";
+}
+//함수 removeZero 정의
+function removeZero(){
+    var value = document.getElementById("output").innerHTML;
+    if (value == "0") {
+        value = " "
+        document.getElementById("output").innerHTML = value;
+    }
+}
+//화면값 출력
+function fordisplay(value) {
+    removeZero()
+    document.getElementById("output").innerHTML += value;
+}
+// %버튼 기능 추가
+function perc() {
+    var value = document.getElementById("output").innerHTML;
+    value = value / 100;
+    document.getElementById("output").innerHTML = value;
+}
--- study/study_calculate/js/script.js
+++
@@ -1,4 +0,0 @@
-// 계산기 자바스크립트
-function forclear() {
-    document.getElementById("output").innerHTML = "0";
-}
