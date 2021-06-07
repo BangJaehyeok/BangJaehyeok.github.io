d3ac742288c2182874f28d69335ffbb2ade7a6db
BangJaehyeok <BangJaehyeok@test.com>
2021-06-07 21:46:40 +0900

    전자시계만들어보기

--- study/study_clock/clock2.js
+++ study/study_clock/clock2.js
@@ -2,18 +2,20 @@ var clock = document.querySelector(".clock")
 
 var clockTitle = clock.querySelector("h1");
 
+//시간표시
 function Time(){
     var date = new Date();
     var hour = date.getHours();
     var minute = date.getMinutes();
     var second = date.getSeconds();
-    clockTitle.innerText = `${hour < 10? `0${hour}`:hour}:${minute}:${second < 10? `0${second}`: second}`;
+    clockTitle.innerText = `${hour < 10? `0${hour}`:hour}:${minute}:${second < 10? `0${second}`: second}`; //초단위를 2자리수로 보이게하기
 }
 function show(){
     Time();
     setInterval(Time,1000);
 }
 show();
+//시계가동하기
 
 
 
